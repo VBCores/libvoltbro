8ecbec825e6270ea5c356879565c4c94057d31c5
Igor Beschastnov <beschastnovigor@gmail.com>
2024-02-12 19:07:26 +0300

    communication additions

--- voltbro/devices/blheli_s.hpp
+++ voltbro/devices/blheli_s.hpp
@@ -7,23 +7,45 @@
 
 class BLHeli_SController {
 private:
-    const uint32_t min_phase;
+    const tim_register min_phase;
     TIM_HandleTypeDef * const htim;
-    const uint32_t channel;
+    const tim_register channel;
+    bool _is_on = false;
 public:
-    static constexpr uint32_t RECOMMENDED_DELAY = 1500;
+    static constexpr millis RECOMMENDED_DELAY = 1500;
 
-    BLHeli_SController(TIM_HandleTypeDef *htim, uint32_t channel, uint32_t period):
+    BLHeli_SController(TIM_HandleTypeDef *htim, tim_register channel, tim_register period):
         min_phase(period / 20), htim(htim), channel(channel)
     {}
 
     void set_pulse(float pulse) {
+        if (!_is_on) {
+            // Защита от дурака
+            __HAL_TIM_SET_COMPARE(htim, channel, 0);
+            return;
+        }
         assert_param((pulse >= 0.0) && (pulse <= 1.0));
         const uint32_t adj = min_phase * pulse;
         __HAL_TIM_SET_COMPARE(htim, channel, min_phase + adj);
     }
 
+    void stop() {
+        set_pulse(0);
+        _is_on = false;
+    }
+
+    float get_pulse() const {
+        const tim_register cpr = __HAL_TIM_GET_COMPARE(htim, channel);
+        const tim_register offset_cpr = cpr - min_phase;
+        return (float)offset_cpr / (float)min_phase;
+    }
+
+    bool is_on() const {
+        return _is_on;
+    }
+
     void start() {
+        _is_on = true;
         set_pulse(0);
         HAL_TIM_PWM_Start(htim, channel);
     }
